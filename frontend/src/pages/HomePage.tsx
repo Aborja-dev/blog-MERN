6559f434f4ce4f365a2e8a524c6dae4780711060
ankahara <hecatombe100@gmail.com>
2024-05-19 23:29:00 -0600

    creacion de la estructura del proyecto

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import reactLogo from "@assets/react.svg";
+const HomePage = () => {
+  return (
+    <div>
+      <img src={reactLogo} alt="" />
+    </div>
+  )
+}
+
+export default HomePage
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+    }
+  }
 })
